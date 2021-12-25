fb3d6ce279f5ea00c9eaaadb13b1f0be9f981990
SakuraWHU <1325444271@qq.com>
2021-12-25 19:17:23 +0800

    add navigator

--- vue-gis/src/views/Index.vue
+++ vue-gis/src/views/Index.vue
@@ -1,7 +1,86 @@
 <template>
-    <div class="Indexs">首页</div>
+    <div>
+        <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+        >
+            <el-sub-menu index="1">
+                <template #title>
+                    <el-icon>
+                        <location />
+                    </el-icon>
+                    <span>Navigator One</span>
+                </template>
+                <el-menu-item-group title="Group One">
+                    <el-menu-item index="1-1">item one</el-menu-item>
+                    <el-menu-item index="1-2">item one</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group Two">
+                    <el-menu-item index="1-3">item three</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="1-4">
+                    <template #title>item four</template>
+                    <el-menu-item index="1-4-1">item one</el-menu-item>
+                </el-sub-menu>
+            </el-sub-menu>
+            <el-menu-item index="2">
+                <el-icon>
+                    <icon-menu />
+                </el-icon>
+                <span>Navigator Two</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+                <el-icon>
+                    <document />
+                </el-icon>
+                <span>Navigator Three</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <el-icon>
+                    <setting />
+                </el-icon>
+                <span>Navigator Four</span>
+            </el-menu-item>
+        </el-menu>
+    </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {
+    Location,
+    Document,
+    Menu as IconMenu,
+    Setting,
+} from '@element-plus/icons-vue'
 
+export default defineComponent({
+    components: {
+        Location,
+        Document,
+        Setting,
+        IconMenu,
+    },
+    setup() {
+        const handleOpen = (key: any, keyPath: any) => {
+            console.log(key, keyPath)
+        }
+        const handleClose = (key: any, keyPath: any) => {
+            console.log(key, keyPath)
+        }
+        return {
+            handleOpen,
+            handleClose,
+        }
+    },
+})
 </script>
+
+<style scoped>
+.el-menu-vertical-demo {
+    width: 200px;
+    min-height: 100vh;
+}
+</style>
