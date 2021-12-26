424117ec940f4ff8ec60ffabf51441baf69b4c96
XianyiWang <826302632@qq.com>
2021-12-26 19:04:38 +0800

    sync

--- vue-gis/src/views/EventMap.vue
+++ vue-gis/src/views/EventMap.vue
@@ -7,17 +7,27 @@
         style="width: 300px; margin-bottom: 10px"
         size="small"
       />
-      <el-button type="primary" @click="searchEvent" style="margin-left: 10px" size="small">查询</el-button>
+      <el-button
+        type="primary"
+        @click="searchEvent"
+        style="margin-left: 10px"
+        size="small"
+        >查询</el-button
+      >
       <el-button
         type="primary"
         plain
         @click="getEventList"
         style="margin-left: 10px"
         size="small"
-      >重置</el-button>
+        >重置</el-button
+      >
     </div>
 
     <div id="container" style="flex: 1"></div>
+    <!-- <iframe style="flex: 1"
+        src="https://maplab.amap.com/share/mapv/22e19a8fb91eefdd5c2bf753e9c5562a"
+      ></iframe> -->
     <div style="margin: 0 auto">
       <el-radio-group v-model="radio" @change="getEventList" size="small">
         <el-radio-button label="all">全部事件</el-radio-button>
@@ -30,35 +40,41 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import { shallowRef } from '@vue/reactivity'
-import AMapLoader from '@amap/amap-jsapi-loader';
+import { onMounted, ref,reactive } from "vue";
+import { shallowRef } from "@vue/reactivity";
+import AMapLoader from "@amap/amap-jsapi-loader";
 // 选择不同的类别
 let input = ref("");
-function searchEvent() { }
+function searchEvent() {}
 
-function getEventList() { }
+function getEventList() {}
 
 let radio = ref("all");
 let map = shallowRef(null);
+let data=reactive({
+    marker:[]
+})
 function initMap() {
   AMapLoader.load({
-    key: "cdbb4acf696f8080139bc1cb2a29000d",             // 申请好的Web端开发者Key，首次调用 load 时必填
-    version: "2.0",      // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-    plugins: [''],       // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-  }).then((AMap) => {
-    map = new AMap.Map("container", {  //设置地图容器id
-      viewMode: "3D",    //是否为3D地图模式
-      zoom: 5,           //初始化地图级别
-      center: [105.602725, 37.076636], //初始化地图中心点位置
-    });
-  }).catch(e => {
-    console.log(e);
+    key: "cdbb4acf696f8080139bc1cb2a29000d", // 申请好的Web端开发者Key，首次调用 load 时必填
+    version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+    plugins: [""], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
   })
+    .then((AMap) => {
+      map = new AMap.Map("container", {
+        //设置地图容器id
+        viewMode: "3D", //是否为3D地图模式
+        zoom: 15, //初始化地图级别
+        center: [114.362378, 30.534268], //初始化地图中心点位置
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
 onMounted(() => {
   initMap();
-})
+});
 </script>
 
 <style scoped>
