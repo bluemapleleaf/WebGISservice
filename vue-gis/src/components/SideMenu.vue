aaeb5cd8148c1679f8c9482d1e8eaf2733e9c811
SakuraWHU <1325444271@qq.com>
2021-12-27 21:38:01 +0800

    change router

--- vue-gis/src/components/SideMenu.vue
+++ vue-gis/src/components/SideMenu.vue
@@ -1,53 +1,53 @@
 <template>
-  <el-menu default-active="/usermanage" class="el-menu-vertical-demo" router>
-    <el-menu-item index="/usermanage">
-      <el-icon>
-        <avatar />
-      </el-icon>
-      <template #title>用户管理</template>
-    </el-menu-item>
+    <el-menu default-active="/usermanage" class="el-menu-vertical-demo" router>
+        <el-menu-item index="/usermanage">
+            <el-icon>
+                <avatar/>
+            </el-icon>
+            <template #title>我的</template>
+        </el-menu-item>
 
-    <el-menu-item index="/eventlist">
-      <el-icon>
-        <document />
-      </el-icon>
-      <template #title>事件列表</template>
-    </el-menu-item>
-    <el-menu-item index="/eventmap">
-      <el-icon>
-        <location />
-      </el-icon>
-      <template #title>事件地图</template>
-    </el-menu-item>
+        <el-menu-item index="/eventlist">
+            <el-icon>
+                <document/>
+            </el-icon>
+            <template #title>事件列表</template>
+        </el-menu-item>
+        <el-menu-item index="/eventmap">
+            <el-icon>
+                <location/>
+            </el-icon>
+            <template #title>事件地图</template>
+        </el-menu-item>
 
-    <el-menu-item index="/announce">
-      <el-icon>
-        <chat-dot-square />
-      </el-icon>
-      <template #title>事件公告发布</template>
-    </el-menu-item>
-    <el-menu-item index="/analysis">
-      <el-icon>
-        <data-analysis />
-      </el-icon>
-      <template #title>可视化分析</template>
-    </el-menu-item>
-  </el-menu>
+        <el-menu-item index="/announce">
+            <el-icon>
+                <chat-dot-square/>
+            </el-icon>
+            <template #title>事件公告发布</template>
+        </el-menu-item>
+        <el-menu-item index="/analysis">
+            <el-icon>
+                <data-analysis/>
+            </el-icon>
+            <template #title>可视化分析</template>
+        </el-menu-item>
+    </el-menu>
 </template>
 
 <script setup lang="ts">
 import {
-  Location,
-  Document,
-  ChatDotSquare,
-  Avatar,
-  DataAnalysis
+    Location,
+    Document,
+    ChatDotSquare,
+    Avatar,
+    DataAnalysis
 } from "@element-plus/icons-vue";
 </script>
 
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 100vh;
+    width: 200px;
+    min-height: 100vh;
 }
 </style>
