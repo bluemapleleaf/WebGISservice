ac850438fd16c2fec8770faf87c51a55c12cd603
XianyiWang <826302632@qq.com>
2021-12-27 16:46:19 +0800

    add eventlist layout

--- vue-gis/src/main.ts
+++ vue-gis/src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import axios from './utils/http'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import axios from "./utils/http";
 
-const app = createApp(App).use(router).use(ElementPlus)
+const app = createApp(App).use(router).use(ElementPlus,{locale:zhCn});
 
-app.mount('#app')
-app.config.globalProperties.$axios = axios
-
+app.mount("#app");
+app.config.globalProperties.$axios = axios;
